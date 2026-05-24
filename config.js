56227e98f735704573084acb33ae77b2ad1c34ca
BD MASUM VAI <0.bdmasumvai@gmail.com>
2026-05-24 12:02:47 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,21 @@
+// config.js - Easily manage your social and button links here
+const CONFIG = {
+    // Contact Information
+    contact: {
+        phone: "+8801617072663",
+        telegram: "https://t.me/masumvai"
+    },
+    
+    // Popup Menu Links (Triggered by any footer social icon)
+    socialPopupLinks: {
+        telegramChannel: "https://t.me/Masum_Tech_Sensei",
+        youtubeChannel: "https://youtube.com/@bdmasumvai",
+        facebookPage: "https://facebook.com/your_page",
+        whatsappCSB: "https://wa.me/8801617072663",
+        instagram: "https://instagram.com/your_username",
+        twitterX: "https://twitter.com/your_username"
+    },
+
+    // Path to your background music
+    musicPath: "music/background.mp3"
+};
